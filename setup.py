a49aff0f842083f8e1d1ccf4b5336df5712ceed1
Leonardo Fonseca Reginato <leonardofonseca.r@gmail.com>
2024-02-26 08:03:43 -0600

    Feat: description in pypi page

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    description = f.read()
+
 if __name__ == "__main__":
-    setup()
+    setup(
+        long_description=description,
+        long_description_content_type="text/markdown",
+    )
